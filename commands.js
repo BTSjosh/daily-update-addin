6a5689ccf93243d4f24ec3786755bfdb00c429b6
BTSjosh <joshplimmer@btsdata.com>
2026-07-07 17:56:13 -0400

    Move camera model list to Camera types installed

--- commands.js
+++ commands.js
@@ -27,13 +27,7 @@ function dailyUpdate(event) {
     '<div style="font-family:Calibri,Arial,sans-serif;font-size:11pt">' +
       '<p><b>Work Completed / In Progress:</b></p>' +
       '<ul>' +
-        '<li>Cameras installed:&nbsp;' +
-          '<ul>' +
-            '<li>QNV-C8083R</li>' +
-            '<li>TNV-8011C</li>' +
-            '<li>QNF-8010</li>' +
-          '</ul>' +
-        '</li>' +
+        '<li>Cameras installed:&nbsp;</li>' +
         '<li>Data / LV cables pulled:&nbsp;</li>' +
         '<li>Penetrations sealed:&nbsp;</li>' +
       '</ul>' +
@@ -44,7 +38,13 @@ function dailyUpdate(event) {
       '</ul>' +
       '<p><b>Scope Details:</b></p>' +
       '<ul>' +
-        '<li>Camera types installed:&nbsp;</li>' +
+        '<li>Camera types installed:&nbsp;' +
+          '<ul>' +
+            '<li>QNV-C8083R</li>' +
+            '<li>TNV-8011C</li>' +
+            '<li>QNF-8010</li>' +
+          '</ul>' +
+        '</li>' +
         '<li>Technicians on site:&nbsp;4</li>' +
       '</ul>' +
       '<p><b>Notes / Constraints:</b></p>' +
